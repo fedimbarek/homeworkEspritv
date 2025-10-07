47bfa6de910f91ef3e25e4cd88eab3cf56b09a94
Fedi Mbarek Abidi <121312608+fedimbarek@users.noreply.github.com>
2025-10-07 21:55:27 +0100

    Update CommandeRepository.java

--- src/main/java/tn/esprit/twin/projetspringclasse/repositores/CommandeRepository.java
+++ src/main/java/tn/esprit/twin/projetspringclasse/repositores/CommandeRepository.java
@@ -1,7 +1,15 @@
 package tn.esprit.twin.projetspringclasse.repositores;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Date;
+import java.util.List;
 import tn.esprit.twin.projetspringclasse.models.Commande;
 
+@Repository
 public interface CommandeRepository extends JpaRepository<Commande, Long> {
+    List<Commande> findByClientId(Long idClient);
+    List<Commande> findByClientIdAndDateCommandeBetween(Long idClient, Date dateDebut, Date dateFin);
+    List<Commande> findByDateCommandeBetweenOrderByNoteDesc(Date dateDebut, Date dateFin);
 }
+
