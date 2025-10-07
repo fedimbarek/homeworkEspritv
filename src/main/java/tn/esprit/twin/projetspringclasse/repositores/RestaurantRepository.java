c556b07e7ec8cb8dcd4b6197f83332b66d4ba13d
Fedi Mbarek Abidi <121312608+fedimbarek@users.noreply.github.com>
2025-10-07 21:57:17 +0100

    Update RestaurantRepository.java

--- src/main/java/tn/esprit/twin/projetspringclasse/repositores/RestaurantRepository.java
+++ src/main/java/tn/esprit/twin/projetspringclasse/repositores/RestaurantRepository.java
@@ -1,7 +1,14 @@
 package tn.esprit.twin.projetspringclasse.repositores;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tn.esprit.twin.projetspringclasse.models.Restaurant;
 
+import java.util.Date;
+import java.util.List;
+
+@Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    List<Restaurant> findByNbPlacesMaxGreaterThanAndChaineRestaurationDateCreationBefore(Long nbPlacesMax, Date date);
+
 }
