60dcac9f4fffd5aa6ba7439cab8a648e3fb9d070
Fedi Mbarek Abidi <121312608+fedimbarek@users.noreply.github.com>
2025-10-07 21:56:27 +0100

    Update MenuRepository.java

--- src/main/java/tn/esprit/twin/projetspringclasse/repositores/MenuRepository.java
+++ src/main/java/tn/esprit/twin/projetspringclasse/repositores/MenuRepository.java
@@ -2,6 +2,10 @@ package tn.esprit.twin.projetspringclasse.repositores;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import tn.esprit.twin.projetspringclasse.models.Menu;
-
+import java.util.List;
+@Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    List<Menu> findByTypeMenuAndComposantsPrixGreaterThan(String typeMenu, Double prix);
+
 }
+
